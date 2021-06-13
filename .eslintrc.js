d7b2920641830a786ba0b9452de264281f6d7a9a
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-06-13 15:58:44 -0300

    feat: mobile structure

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => (
+  <View>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+    <Text>Testereee</Text>
+  </View>
+)
+
+export default App
