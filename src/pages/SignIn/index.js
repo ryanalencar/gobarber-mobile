8df666e087e840a3139c1318e9d59745fc877cb6
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-06-13 16:56:09 -0300

    feat: button component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Text } from 'react-native'
 
 import Background from '../../components/Background'
+import Button from '../../components/Button'
 import Input from '../../components/Input'
 
 function SignIn() {
   return (
     <Background>
       <Text> SignIn Works!!!</Text>
-      <Input icon='call' placeholder='Digite seu telefone' />
+      <Input icon='add-chart' placeholder='Digite seu telefone' />
+      <Button>Teste</Button>
     </Background>
   )
 }
