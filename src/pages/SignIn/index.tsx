e07478bb4ef835f105ad160306b8981aba395f80
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-07-15 18:21:49 -0300

    feat: sign in page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Image, Text } from 'react-native'
 
 import Background from '../../components/Background'
-import Button from '../../components/Button'
-import Input from '../../components/Input'
 
-import { Container } from './styles'
+import {
+  Container,
+  FormInput,
+  SubmitButton,
+  Form,
+  SignLinkText,
+} from './styles'
+import logo from '../../assets/logo.png'
 
 function SignIn() {
   return (
     <Background>
-      <Container />
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon='mail-outline'
+            keyboardType='email-address'
+            autoCorrect={false}
+            autoCapitalize='none'
+            placeholder='Digite seu email'
+          />
+          <FormInput
+            icon='lock-outline'
+            secureTextEntry
+            placeholder='Digite sua senha secreta'
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        {/* <SignLink>
+          <SignLinkText />
+        </SignLink> */}
+      </Container>
     </Background>
   )
 }
