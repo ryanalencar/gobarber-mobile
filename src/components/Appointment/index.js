2021277ba0209ab5ccffed455bca5246a6589d4f
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-07-22 15:05:35 -0300

    feat: appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,28 +1,51 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import PropTypes from 'prop-types'
+import { parseISO, formatRelative } from 'date-fns'
+import pt from 'date-fns/locale/pt'
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles'
 
-function Appointment() {
+function Appointment({ data }) {
+  const { date, provider, past, cancelable, onCancel, canceled_at } = data
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    })
+  }, [date])
+
   return (
-    <Container>
+    <Container past={past}>
       <Left>
         <Avatar
           source={{
-            uri: 'https://pics.freeicons.io/uploads/icons/png/6882831431553666420-512.png',
+            uri: provider.avatar
+              ? provider.avatar_url
+              : 'https://pics.freeicons.io/uploads/icons/png/6882831431553666420-512.png',
           }}
         />
         <Info>
-          <Name>Ryan Alencar</Name>
-          <Time>em 3 horas</Time>
+          <Name>{provider.name}</Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name='event-busy' size={20} color='#f64c75' />
-      </TouchableOpacity>
+      {cancelable && !canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name='event-busy' size={20} color='#f64c75' />
+        </TouchableOpacity>
+      )}
     </Container>
   )
+}
+
+Appointment.defaultProps = {
+  data: {},
+}
+
+Appointment.propTypes = {
+  data: PropTypes.object,
 }
 
 export default Appointment
