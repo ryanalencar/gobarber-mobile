f52663df1175494cbe95d850ae41028e38ca82c7
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2021-08-02 16:48:14 -0300

    feat: confirm appointment

--- src/pages/New/ConfirmAppointment/index.js
+++ src/pages/New/ConfirmAppointment/index.js
@@ -1,10 +1,60 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useMemo } from 'react'
+import { TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import { formatRelative, parseISO } from 'date-fns'
+import pt from 'date-fns/locale/pt'
 
-// import { Container } from './styles';
+import api from '../../../services/api'
+import Background from '../../../components/Background'
+import { Container, Avatar, Name, Time, SubmitButton } from './styles'
 
-function ConfirmAppointment() {
-  return <Text>ConfirmAppointment</Text>
+export default function ConfirmAppointment({ navigation }) {
+  const provider = navigation.getParam('provider')
+  const time = navigation.getParam('time')
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  )
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    })
+
+    navigation.navigate('Dashboard')
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar_url
+              : 'https://pics.freeicons.io/uploads/icons/png/6882831431553666420-512.png',
+          }}
+        />
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  )
 }
 
-export default ConfirmAppointment
+ConfirmAppointment.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar agendamento',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack()
+      }}
+    >
+      <Icon name='chevron-left' size={20} color='#fff' />
+    </TouchableOpacity>
+  ),
+})
